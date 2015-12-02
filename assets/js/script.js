0d17aef83df14afcf17f6cc5e7d7c133b947e9d2
Eric Bo <ericbo@berkeley.edu>
2015-12-02 14:56:17 -0800

    added sliding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,42 @@
 $(document).ready(function() {
 
-  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
-  // $("#sidebar-button").click(function() {  //(When the user clicks on an the sidebar button)
-  //   if($(".sidebar-container").hasClass("sidebar-active")) { //If the sidebar container has the "active" class:
-  //     $("body").removeClass("no-scroll"); //Remove the "no-scroll" class from the body tag
-  //     $("#sidebar-button").removeClass("button-active"); //Remove the "active" class from the sidebar button
-  //     $(".sidebar-container").removeClass("sidebar-active"); //Remove the "active" class from the sidebar container
-  //     $(".page-wrapper").removeClass("wrapper-active"); //Remove the "active" class from the page wrapper
-  //   }
-  //   else { //Otherwise:
-  //     $("#sidebar-button").addClass("button-active"); //Add the "active" class to sidebar button
-  //     $(".sidebar-container").addClass("sidebar-active"); //Add the "active" class to sidebar container
-  //     $(".page-wrapper").addClass("wrapper-active"); //Add the "active" class to the page wrapper
-  //     //Add the "no-scroll" class to the body tag with a delay of 300 ms (code above)
-  //     setTimeout(function() {
-  //       $('body').addClass('no-scroll');
-  //     }, 300);
-  //   }
-  // });
-  
-  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
-  // $(".page-wrapper").click(function() {
-  //   if($(".sidebar-container").hasClass("sidebar-active")) {
-  //     $("body").removeClass("no-scroll");
-  //     $("#sidebar-button").removeClass("button-active");
-  //     $(".sidebar-container").removeClass("sidebar-active");
-  //     $(".page-wrapper").removeClass("wrapper-active");
-  //   }
-  // });
+  $('#left-personal').click(function() {
+    $('.personal-panel').addClass("personal-panel-small");
+    $('.professional-panel').addClass("professional-panel-big");
 
-  //Implement the "slide to left" when the user clicks on #carousel-next here
-  //   $("#carousel-next").click(function() {
-  //   var cur = parseInt($('#carousel').css('margin-left').replace("px", ""));
-  //   if (cur == -3840) {
-  //     return false;
-  //   }
-  //   else {
-  //     $('#carousel').css('margin-left', cur - 960 + "px");
-  //   }
-  // });
+    $('.info-container-personal').removeClass("hidden");
+    $('#left-personal').addClass("hidden");
+    $('#right-professional').addClass("hidden");
+  });
 
-  //Implement the "slide to right" when the user clicks on #carousel-prev here
-  // $("#carousel-prev").click(function() {
-  //   var cur = parseInt($('#carousel').css('margin-left').replace("px", ""));
-  //   if (cur == 0) {
-  //     return false;
-  //   }
-  //   else {
-  //     $('#carousel').css('margin-left', cur + 960 + "px");
-  //   }
-  // });
+  $('#right-professional').click(function() {
+    $('.personal-panel').addClass("personal-panel-big");
+    $('.professional-panel').addClass("professional-panel-small");
+
+    $('.info-container-professional').removeClass("hidden");
+    $('#right-professional').addClass("hidden");
+    $('#left-personal').addClass("hidden");
+  });
+
+  $('#right-personal').click(function() {
+    $('.personal-panel').addClass("personal-panel");
+    $('.personal-panel').removeClass("personal-panel-small");
+    $('.professional-panel').addClass("professional-panel");
+    $('.professional-panel').removeClass("professional-panel-big");
+
+    $('.info-container-personal').addClass("hidden");
+    $('#left-personal').removeClass("hidden");
+    $('#right-professional').removeClass("hidden");
+  });
+
+  $('#left-professional').click(function() {
+    $('.personal-panel').addClass("personal-panel");
+    $('.personal-panel').removeClass("personal-panel-big");
+    $('.professional-panel').addClass("professional-panel");
+    $('.professional-panel').removeClass("professional-panel-small");
+
+    $('.info-container-professional').addClass("hidden");
+    $('#right-professional').removeClass("hidden");
+    $('#left-personal').removeClass("hidden");
+  });
 });
