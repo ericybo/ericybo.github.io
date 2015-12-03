399c0bc22696b0675271a1de3495d5485e8660ef
Eric Bo <ericbo@berkeley.edu>
2015-12-02 18:05:10 -0800

    added basic info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,46 @@
 $(document).ready(function() {
 
   $('#left-personal').click(function() {
-    $('.personal-panel').addClass("personal-panel-small");
-    $('.professional-panel').addClass("professional-panel-big");
-
-    $('.info-container-personal').removeClass("hidden");
-    $('#left-personal').addClass("hidden");
-    $('#right-professional').addClass("hidden");
-  });
-
-  $('#right-professional').click(function() {
     $('.personal-panel').addClass("personal-panel-big");
     $('.professional-panel').addClass("professional-panel-small");
 
+    $('.info-container-personal').removeClass("hidden");
+    $('#left-professional').removeClass("hidden");
+    $('#left-personal').addClass("hidden");
+    $('#right-professional').addClass("hidden");
+    $('#personal').addClass("hidden");
+  });
+
+  $('#right-professional').click(function() {
+    $('.personal-panel').addClass("personal-panel-small");
+    $('.professional-panel').addClass("professional-panel-big");
+
     $('.info-container-professional').removeClass("hidden");
+    $('#right-personal').removeClass("hidden");
     $('#right-professional').addClass("hidden");
     $('#left-personal').addClass("hidden");
+    $('#professional').addClass("hidden");
   });
 
   $('#right-personal').click(function() {
-    $('.personal-panel').addClass("personal-panel");
     $('.personal-panel').removeClass("personal-panel-small");
-    $('.professional-panel').addClass("professional-panel");
     $('.professional-panel').removeClass("professional-panel-big");
 
-    $('.info-container-personal').addClass("hidden");
-    $('#left-personal').removeClass("hidden");
+    $('.info-container-professional').addClass("hidden");
+    $('#right-personal').addClass("hidden");
     $('#right-professional').removeClass("hidden");
+    $('#left-personal').removeClass("hidden");
+    $('#professional').removeClass("hidden");
   });
 
   $('#left-professional').click(function() {
-    $('.personal-panel').addClass("personal-panel");
     $('.personal-panel').removeClass("personal-panel-big");
-    $('.professional-panel').addClass("professional-panel");
     $('.professional-panel').removeClass("professional-panel-small");
 
-    $('.info-container-professional').addClass("hidden");
-    $('#right-professional').removeClass("hidden");
+    $('.info-container-personal').addClass("hidden");
+    $('#left-professional').addClass("hidden");
     $('#left-personal').removeClass("hidden");
+    $('#right-professional').removeClass("hidden");
+    $('#personal').removeClass("hidden");
   });
 });
